fff6f3734c8e66991c30cc34448e5174022ada23
OkyRuiz <159487334+OkyRuiz@users.noreply.github.com>
2026-07-12 23:32:58 -0500

    Lab #7

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -1,7 +1,9 @@
 """
 Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 """
-
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
 # pylint: disable=import-outside-toplevel
 
 
@@ -16,3 +18,82 @@ def pregunta_01():
     El gráfico debe salvarse al archivo `files/plots/news.png`.
 
     """
+
+    df = pd.read_csv("files/input/news.csv", index_col=0)
+
+    colors = {
+        "Television": "dimgrey",
+        "Newspaper": "grey",
+        "Radio": "lightgrey",
+        "Internet": "tab:blue",
+    }
+
+    zorder = {
+        "Television": 1,
+        "Newspaper": 1,
+        "Radio": 1,
+        "Internet": 2,
+    }
+
+    linewidth = {
+        "Television": 1,
+        "Newspaper": 1,
+        "Radio": 1,
+        "Internet": 4,
+    }
+
+    plt.figure(figsize=(6, 4))
+
+    for col in df.columns:
+        plt.plot(
+            df[col],
+            label=col,
+            color=colors[col],
+            zorder=zorder[col],
+            linewidth=linewidth[col],
+        )
+
+    plt.title("People get news from", fontsize=16)
+
+    ax = plt.gca()
+    ax.spines["top"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+    ax.spines["left"].set_visible(False)
+    ax.yaxis.set_visible(False)
+
+    first_year = df.index[0]
+    last_year = df.index[-1]
+
+    for col in df.columns:
+        first_value = df[col].iloc[0]
+        last_value = df[col].iloc[-1]
+
+        plt.scatter(first_year, first_value, color=colors[col])
+        plt.text(
+            first_year - 0.2,
+            first_value,
+            f"{col}  {first_value}%",
+            color=colors[col],
+            ha="right",
+            va="center",
+        )
+
+        plt.scatter(last_year, last_value, color=colors[col])
+        plt.text(
+            last_year + 0.2,
+            last_value,
+            f"{last_value}%",
+            color=colors[col],
+            ha="left",
+            va="center",
+        )
+
+    plt.xticks(df.index, [str(y) for y in df.index], ha="center")
+    plt.tight_layout()
+
+    os.makedirs("files/plots", exist_ok=True)
+    plt.savefig("files/plots/news.png")
+    plt.close()
+
+if __name__ == "__main__":
+    pregunta_01()
